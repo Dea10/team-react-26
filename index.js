3b0541ae41909ef701e97324a58cec72f6db99c3
Daniel Espinosa <d111092.de@gmail.com>
2021-01-24 16:33:20 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./src/schema");
+const resolvers = require("./src/resolvers");
+const expressPlayground = require("graphql-playground-middleware-express").default;
+
+// Create a context for holding contextual data 
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+  })
+);
+
+//Graphql Playground route
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+
+const port = process.env.PORT || "4000";
+
+app.listen(port);
+
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
